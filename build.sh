d31830d2b4d885c7cda277fb130c01a7e110ef66
SrMotion - sadMotion <66473364+SrMotion@users.noreply.github.com>
2021-03-24 15:50:49 +0300

    build linux
    linux build

---
+++ build.sh
@@ -0,0 +1,2 @@
+g++ -std=c++17 -Ofast -pthread -o proxy_linux proxy/enet/callbacks.c proxy/enet/compress.c proxy/enet/host.c proxy/enet/list.c proxy/enet/packet.c proxy/enet/peer.c proxy/enet/protocol.c proxy/enet/unix.c proxy/enet/win32.c proxy/events.cpp proxy/gt.cpp proxy/http.cpp proxy/proxy.cpp proxy/sandbird/sandbird.c proxy/server.cpp proxy/utils.cpp
+
