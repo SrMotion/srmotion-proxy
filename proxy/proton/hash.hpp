aed481d5471c9c5ebf0c304807110bfdc9aeb18d
SrMotion - sadMotion <66473364+SrMotion@users.noreply.github.com>
2021-03-24 15:57:41 +0300

    Update hash.hpp

--- proxy/proton/hash.hpp
+++ proxy/proton/hash.hpp
@@ -3,6 +3,13 @@
 
 #pragma warning(disable : 4307)
 
+#ifdef _WIN32
+#define INLINE __forceinline
+#else //for gcc/clang
+#define INLINE __attribute__((always_inline))
+#endif
+
+
 template <typename t, t val>
 class cexpr {
    public:
@@ -13,10 +20,10 @@ namespace hs {
     constexpr uint32_t prime_32_const = 0x1000193u;
     constexpr uint64_t val_64_const = 0xcbf29ce484222325u;
     constexpr uint64_t prime_64_const = 0x100000001b3u;
-    __forceinline constexpr uint32_t hash32(const char* const str, const uint32_t value = val_32_const) noexcept {
+    INLINE constexpr uint32_t hash32(const char* const str, const uint32_t value = val_32_const) noexcept {
         return (str[0] == '\0') ? value : hash32(&str[1], (value ^ uint32_t(str[0])) * prime_32_const);
     }
-    __forceinline constexpr uint64_t hash64(const char* const str, const uint64_t value = val_64_const) noexcept {
+    INLINE constexpr uint64_t hash64(const char* const str, const uint64_t value = val_64_const) noexcept {
         return (str[0] == '\0') ? value : hash64(&str[1], (value ^ uint64_t(str[0])) * prime_64_const);
     }
 } // namespace hs
