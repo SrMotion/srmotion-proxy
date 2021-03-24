c807b225725516e93aa125a3921c185ce83d2ce7
SrMotion - sadMotion <66473364+SrMotion@users.noreply.github.com>
2021-03-24 15:48:23 +0300

    version update
    version update

--- proxy/gt.cpp
+++ proxy/gt.cpp
@@ -4,7 +4,7 @@
 #include "server.h"
 #include "utils.h"
 
-std::string gt::version = "3.57";
+std::string gt::version = "4.20";//version update
 std::string gt::flag = "tr";
 bool gt::resolving_uid2 = false;
 bool gt::connecting = false;
