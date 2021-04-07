b5b011f2278707995b7d04eaf735c65077c5d11c
SrMotion - sadMotion <66473364+SrMotion@users.noreply.github.com>
2021-04-07 21:03:19 +0300

    Update server.h

--- proxy/server.h
+++ proxy/server.h
@@ -31,7 +31,7 @@ class server {
     int m_user = 0;
     int m_token = 0;
     std::string m_server = "213.179.209.168";
-    int m_port = 17198;
+    int m_port = 17201;
     std::string serverz = m_server;
     int portz = m_port;
     int m_proxyport = 17191;
